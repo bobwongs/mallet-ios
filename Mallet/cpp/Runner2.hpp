f0accebf0971034d373d217a4a5545a9743c6c60
Katsu Matsuda <kats.mtd@gmail.com>
2019-06-26 00:24:59 +0900

    Add runner

---
+++ Mallet/cpp/Runner2.hpp
@@ -0,0 +1,19 @@
+//
+//  Runner2.hpp
+//  Mallet
+//
+//  Created by Katsu Matsuda on 2019/06/24.
+//  Copyright © 2019 Katsu Matsuda. All rights reserved.
+//
+
+#ifndef Runner2_h
+#define Runner2_h
+
+#include "cpp.hpp"
+
+typedef struct
+{
+    Runner2 &runner;
+} ArgData;
+
+#endif /* Runner2_h */
--- Mallet/cpp/cpp.hpp
+++ Mallet/cpp/cpp.hpp
@@ -12,59 +12,81 @@
 #include <set>
 #include <string>
 #include <vector>
+#include <array>
 #include <unordered_map>
 
 class CmdID
 {
 public:
+    //* New
+    static constexpr int Push = 90000;
+    static constexpr int Pop = 90001;
+    static constexpr int NumberType = 90002;
+    static constexpr int StringType = 90003;
+    static constexpr int CallCppFunc = 90004;
+    static constexpr int CallMalletFunc = 90005;
+    static constexpr int SetNumberVariable = 90006;
+    static constexpr int SetStringVariable = 90007;
+
+    static constexpr int PrintNumber = 90008;
+    static constexpr int PrintString = 90009;
+
     //* IO
-    static constexpr int Print = 1000;
-    static constexpr int Input = 1001;
+    static constexpr int Print = 10000;
+    static constexpr int Input = 10001;
 
     //* Control
-    static constexpr int Do = 2000;
-    static constexpr int If = 2001;
-    static constexpr int Repeat = 2002;
-    static constexpr int While = 2003;
+    static constexpr int Do = 20000;
+    static constexpr int If = 20001;
+    static constexpr int Repeat = 20002;
+    static constexpr int While = 20003;
 
     //* Variable
-    static constexpr int DeclareIntVariable = 3000;
-    static constexpr int AssignIntVariable = 3001;
-    static constexpr int IntVariableValue = 3002;
-    static constexpr int IntValue = 3003;
-    static constexpr int StringVariableValue = 3004;
-    static constexpr int DeclareStringVariable = 3006;
-    static constexpr int AssignStringVariable = 3007;
+    static constexpr int DeclareIntVariable = 30000;
+    static constexpr int AssignIntVariable = 30001;
+    static constexpr int IntVariableValue = 30002;
+    static constexpr int IntValue = 30003;
+    static constexpr int StringVariableValue = 30004;
+    static constexpr int DeclareStringVariable = 30006;
+    static constexpr int AssignStringVariable = 30007;
 
-    static constexpr int DeclareIntGlobalVariable = 3100;
-    static constexpr int AssignIntGlobalVariable = 3101;
-    static constexpr int IntGlobalVariableValue = 3102;
-    static constexpr int StringGlobalVariableValue = 3104;
-    static constexpr int DeclareStringGlobalVariable = 3106;
-    static constexpr int AssignStringGlobalVariable = 3107;
+    static constexpr int DeclareIntGlobalVariable = 31000;
+    static constexpr int AssignIntGlobalVariable = 31001;
+    static constexpr int IntGlobalVariableValue = 31002;
+    static constexpr int StringGlobalVariableValue = 31004;
+    static constexpr int DeclareStringGlobalVariable = 31006;
+    static constexpr int AssignStringGlobalVariable = 31007;
 
-    static constexpr int AssignIntTmpVariable = 3201;
-    static constexpr int IntTmpVariableValue = 3202;
+    static constexpr int AssignIntTmpVariable = 32001;
+    static constexpr int IntTmpVariableValue = 32002;
 
     //* Operation
-    static constexpr int Add = 4000;
-    static constexpr int Sub = 4001;
-    static constexpr int Mul = 4002;
-    static constexpr int Div = 4003;
-    static constexpr int Mod = 4004;
-    static constexpr int Equal = 4005;
-    static constexpr int NotEqual = 4006;
-    static constexpr int GreaterThan = 4007;
-    static constexpr int LessThan = 4008;
-    static constexpr int GreaterThanOrEqual = 4009;
-    static constexpr int LessThanOrEqual = 4010;
-    static constexpr int And = 4011;
-    static constexpr int Or = 4012;
-    static constexpr int Not = 4013;
+    static constexpr int Add = 40000;
+    static constexpr int Sub = 40001;
+    static constexpr int Mul = 40002;
+    static constexpr int Div = 40003;
+    static constexpr int Mod = 40004;
+    static constexpr int Equal = 40005;
+    static constexpr int NotEqual = 40006;
+    static constexpr int GreaterThan = 40007;
+    static constexpr int LessThan = 40008;
+    static constexpr int GreaterThanOrEqual = 40009;
+    static constexpr int LessThanOrEqual = 40010;
+    static constexpr int And = 40011;
+    static constexpr int Or = 40012;
+    static constexpr int Not = 40013;
 
     //* UI
-    static constexpr int SetUIText = 5000;
+    static constexpr int SetUIText = 50000;
 };
+
+typedef struct
+{
+    int id;
+    int type;
+    int value;
+    int argNum;
+} ByteCode;
 
 class Converter
 {
@@ -95,6 +117,22 @@ public:
     void RunCode(int id, Runner &runner);
 
     void InitRunner(Runner &runner);
+
+    //void InitRunner(std::vector<std::vector<int>> codes, std::vector<std::vector<std::string>> stringVariableInitialValues);
+};
+
+class Runner2
+{
+public:
+    std::vector<int> numberGlobalVariable;
+    std::vector<std::string> stringGlobalVariable;
+
+    std::vector<std::vector<int>> codes;
+    std::vector<std::vector<std::string>> stringVariableInitialValues;
+
+    void RunCode(std::vector<ByteCode> byteCode, Runner2 &runner);
+
+    void InitRunner(Runner2 &runner);
 
     //void InitRunner(std::vector<std::vector<int>> codes, std::vector<std::vector<std::string>> stringVariableInitialValues);
 };
