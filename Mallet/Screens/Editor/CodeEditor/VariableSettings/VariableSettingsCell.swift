0286a1347a14088cce33a3f6bc0ed3d14e7b5fc0
Katsu Matsuda <kats.mtd@gmail.com>
2019-09-29 18:43:32 +0900

    Add VariableSettings

---
+++ Mallet/Screens/Editor/CodeEditor/VariableSettings/VariableSettingsCell.swift
@@ -0,0 +1,24 @@
+//
+//  VariableSettingsCellTableViewCell.swift
+//  Mallet
+//
+//  Created by Katsu Matsuda on 2019/09/29.
+//  Copyright © 2019 Katsu Matsuda. All rights reserved.
+//
+
+import UIKit
+
+class VariableSettingsCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
