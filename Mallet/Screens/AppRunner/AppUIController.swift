6f9c4766da65139b6850c1e1447c8e14d6017fac
Katsu Matsuda <kats.mtd@gmail.com>
2019-10-01 22:38:52 +0900

    Add AppUIController.swift

---
+++ Mallet/Screens/AppRunner/AppUIController.swift
@@ -0,0 +1,13 @@
+//
+//  AppUIController.swift
+//  Mallet
+//
+//  Created by Katsu Matsuda on 2019/10/01.
+//  Copyright © 2019 Katsu Matsuda. All rights reserved.
+//
+
+import UIKit
+
+class AppUIController {
+
+}
