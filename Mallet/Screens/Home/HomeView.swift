4c36493a0313acca7d1b81041c199afb78e363c6
Katsu Matsuda <kats.mtd@gmail.com>
2019-10-28 23:15:43 +0900

    Add HomeView.swift

---
+++ Mallet/Screens/Home/HomeView.swift
@@ -0,0 +1,21 @@
+//
+//  HomeView.swift
+//  Mallet
+//
+//  Created by Katsu Matsuda on 2019/10/28.
+//  Copyright © 2019 Katsu Matsuda. All rights reserved.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Spacer()
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
