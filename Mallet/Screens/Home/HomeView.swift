80ab8d074ecdc7856b256286a44e7f1086654984
Katsu Matsuda <kats.mtd@gmail.com>
2019-10-30 00:17:49 +0900

    Update Home UI

--- Mallet/Screens/Home/HomeView.swift
+++ Mallet/Screens/Home/HomeView.swift
@@ -8,14 +8,66 @@
 
 import SwiftUI
 
+struct HomeAppCell: View {
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Untitled App")
+                    .font(.title)
+                    .fontWeight(.bold)
+                Spacer()
+            }
+            HStack {
+                Spacer()
+                Button(action: {
+                    print("Edit")
+                }) {
+                    Image(systemName: "pencil")
+                }
+            }
+
+        }
+            .frame(maxWidth: .infinity)
+            .padding(15)
+            .background(Color(UIColor.systemFill))
+            .cornerRadius(10)
+    }
+}
+
 struct HomeView: View {
     var body: some View {
-        Spacer()
+        NavigationView {
+            VStack {
+                ScrollView {
+                    VStack {
+                        HomeAppCell()
+                    }
+                        .padding(10)
+                        .padding(.top, 20)
+                }
+                    .navigationBarTitle("My Apps", displayMode: .large)
+                    .navigationBarItems(trailing:
+                            HStack {
+                                Button(action: {
+                                    print(114514)
+                                }) {
+                                    Image(systemName: "plus")
+                                }
+                        }
+                    )
+            }
+        }
     }
 }
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
-        HomeView()
+        Group {
+            HomeView()
+                .colorScheme(.dark)
+
+            HomeView()
+                .colorScheme(.light)
+        }
     }
 }
