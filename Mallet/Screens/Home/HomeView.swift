efcfda787d9b0ef25fec8dec126bafa6044a6381
Katsu Matsuda <kats.mtd@gmail.com>
2019-10-30 23:41:15 +0900

    Update HomeView.swift

--- Mallet/Screens/Home/HomeView.swift
+++ Mallet/Screens/Home/HomeView.swift
@@ -9,39 +9,61 @@
 import SwiftUI
 
 struct HomeAppCell: View {
-    var body: some View {
-        VStack {
-            HStack {
-                Text("Untitled App")
-                    .font(.title)
-                    .fontWeight(.bold)
-                Spacer()
-            }
-            HStack {
-                Spacer()
-                Button(action: {
-                    print("Edit")
-                }) {
-                    Image(systemName: "pencil")
-                }
-            }
 
+    var appID: Int
+
+    var appName: String
+
+    var body: some View {
+        Button(action: {
+            print("Run App")
+        }) {
+            VStack {
+                HStack {
+                    Text(self.appName)
+                        .foregroundColor(Color(UIColor.label))
+                        .font(.title)
+                        .fontWeight(.bold)
+
+                    Spacer()
+                }
+                HStack {
+                    Spacer()
+                    NavigationLink(destination: EditorView()) {
+                        Image(systemName: "pencil")
+                    }
+                }
+
+            }
+                .frame(maxWidth: .infinity)
+                .padding(15)
+                .background(Color(UIColor.systemFill))
+                .cornerRadius(10)
         }
-            .frame(maxWidth: .infinity)
-            .padding(15)
-            .background(Color(UIColor.systemFill))
-            .cornerRadius(10)
+    }
+
+    func moveToEditor() {
+
     }
 }
 
 struct HomeView: View {
+
+    struct app {
+        let appID: Int
+        let appName: String
+    }
+
+    @State var appData = [app]()
+
     var body: some View {
         NavigationView {
             VStack {
                 ScrollView {
                     VStack {
-                        HomeAppCell()
-                        HomeAppCell()
+                        ForEach(self.appData, id: \.appID) { appData in
+                            HomeAppCell(appID: appData.appID, appName: appData.appName)
+                        }
 
                     }
                         .padding(10)
@@ -60,6 +82,13 @@ struct HomeView: View {
             }
         }
             .navigationViewStyle(StackNavigationViewStyle())
+            .onAppear(perform: {
+                self.reloadAppData()
+            })
+    }
+
+    func reloadAppData() {
+        self.appData = [app(appID: 0, appName: "Awesome App"), app(appID: 1, appName: "Cool App")]
     }
 }
 
