bc0d6f419d4f180f36309c0b316f591841aad84b
Katsu Matsuda <kats.mtd@gmail.com>
2019-10-30 23:39:58 +0900

    Add EditorView.swift

---
+++ Mallet/Screens/Editor/EditorView.swift
@@ -0,0 +1,21 @@
+//
+//  EditorView.swift
+//  Mallet
+//
+//  Created by Katsu Matsuda on 2019/10/30.
+//  Copyright © 2019 Katsu Matsuda. All rights reserved.
+//
+
+import SwiftUI
+
+struct EditorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EditorView_Previews: PreviewProvider {
+    static var previews: some View {
+        EditorView()
+    }
+}
