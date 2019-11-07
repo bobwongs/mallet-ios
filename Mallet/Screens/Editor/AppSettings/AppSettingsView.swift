8e21cff023f87128a7c79468a059df8cd51d26f1
Katsu Matsuda <kats.mtd@gmail.com>
2019-11-07 22:52:17 +0900

    Add AppSettingsView

---
+++ Mallet/Screens/Editor/AppSettings/AppSettingsView.swift
@@ -0,0 +1,21 @@
+//
+//  AppSettingsView.swift
+//  Mallet
+//
+//  Created by Katsu Matsuda on 2019/11/07.
+//  Copyright © 2019 Katsu Matsuda. All rights reserved.
+//
+
+import SwiftUI
+
+struct AppSettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AppSettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        AppSettingsView()
+    }
+}
