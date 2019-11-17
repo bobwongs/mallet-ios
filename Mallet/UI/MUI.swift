be677332677f9dd98f7747e74c785f84fd817b6c
Katsu Matsuda <kats.mtd@gmail.com>
2019-11-17 20:19:30 +0900

    Add MUI.swift

---
+++ Mallet/UI/MUI.swift
@@ -0,0 +1,24 @@
+//
+//  MUI.swift
+//  Mallet
+//
+//  Created by Katsu Matsuda on 2019/11/15.
+//  Copyright © 2019 Katsu Matsuda. All rights reserved.
+//
+
+import Foundation
+
+struct MUI: Codable {
+
+    let uiID: Int
+
+    let uiName: String
+
+    let x: Float
+
+    let y: Float
+
+    let width: Float
+
+    let height: Float
+}
