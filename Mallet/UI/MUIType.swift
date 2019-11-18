7c5f6e868dd5c3a5553111f5d5978f2048989ce6
Katsu Matsuda <kats.mtd@gmail.com>
2019-11-18 23:57:24 +0900

    Add MUIType.swift

---
+++ Mallet/UI/MUIType.swift
@@ -0,0 +1,23 @@
+//
+//  MUIType.swift
+//  Mallet
+//
+//  Created by Katsu Matsuda on 2019/11/18.
+//  Copyright © 2019 Katsu Matsuda. All rights reserved.
+//
+
+import Foundation
+
+enum MUIType {
+
+    case Text
+
+    case Button
+
+    case Input
+
+    case Slider
+
+    case Toggle
+
+}
