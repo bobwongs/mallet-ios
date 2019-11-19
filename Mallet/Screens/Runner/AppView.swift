feb89e11122162903a3160123b2a55c9599bcaf6
Katsu Matsuda <kats.mtd@gmail.com>
2019-11-19 22:03:28 +0900

    Add AppView

---
+++ Mallet/Screens/Runner/AppView.swift
@@ -0,0 +1,25 @@
+//
+//  AppView.swift
+//  Mallet
+//
+//  Created by Katsu Matsuda on 2019/11/19.
+//  Copyright © 2019 Katsu Matsuda. All rights reserved.
+//
+
+import SwiftUI
+
+struct AppView: View {
+    var body: some View {
+        ZStack {
+            Spacer()
+        }
+    }
+}
+
+struct AppView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            AppView()
+        }
+    }
+}
