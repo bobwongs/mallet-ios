9a02080592fa0b91727264817e86b00c5eb10737
Katsu Matsuda <kats.mtd@gmail.com>
2019-11-20 22:47:58 +0900

    Update AppView

--- Mallet/Screens/Runner/AppView.swift
+++ Mallet/Screens/Runner/AppView.swift
@@ -12,10 +12,24 @@ struct AppView: View {
 
     let appName: String
 
+    @State var uiData: [MUI] = [MUI(uiID: 0, uiName: "Text1", uiType: .text, x: 100, y: 100, width: 100, height: 50)]
+
     var body: some View {
         NavigationView {
             ZStack {
-                Text("Text")
+                ForEach(0..<uiData.count, content: { (index: Int) -> AnyView in
+
+                    let mui = self.uiData[index]
+
+                    switch mui.uiType {
+
+                    case .text:
+                        return AnyView(MUITextView())
+
+                    default:
+                        fatalError()
+                    }
+                })
             }
                 .navigationBarTitle(Text(appName), displayMode: .inline)
         }
