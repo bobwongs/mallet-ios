72979ed5841324ca18f699991df211c1dfe43900
Katsu Matsuda <kats.mtd@gmail.com>
2019-11-27 23:49:06 +0900

    Add ContentView.swift

---
+++ Mallet/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Mallet
+//
+//  Created by Katsu Matsuda on 2019/11/27.
+//  Copyright © 2019 Katsu Matsuda. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        NavigationView {
+            HomeView()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
