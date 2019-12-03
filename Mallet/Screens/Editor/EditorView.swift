c39bf777e2013804b875b17b7f76d3aaecdcf34b
Katsu Matsuda <kats.mtd@gmail.com>
2019-12-03 23:29:55 +0900

    Update EditorView
    Fix bar button size

--- Mallet/Screens/Editor/EditorView.swift
+++ Mallet/Screens/Editor/EditorView.swift
@@ -31,20 +31,28 @@ struct EditorView: View {
             .edgesIgnoringSafeArea(.bottom)
             .navigationBarTitle("App Title", displayMode: .inline)
             .navigationBarItems(trailing:
-                    HStack {
+                    HStack (alignment: .center) {
+
                         Button(action: {
                             self.showingAppSettingsView = true
                         }) {
                             Image(systemName: "square.and.arrow.up")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
                         }
                             .sheet(isPresented: self.$showingAppSettingsView) {
                                 AppSettingsView()
                         }
 
+                        Spacer()
+                            .frame(width: 20)
+
                         Button(action: {
                             print("Run")
                         }) {
                             Image(systemName: "play.fill")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
                         }
                 }
             )
