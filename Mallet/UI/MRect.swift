197f839c2ed57cc4e7484327d8f20bc5b8de257e
Katsu Matsuda <kats.mtd@gmail.com>
2019-12-18 20:20:26 +0900

    Add MRect.swift

---
+++ Mallet/UI/MRect.swift
@@ -0,0 +1,18 @@
+//
+// Created by Katsu Matsuda on 2019/12/18.
+// Copyright (c) 2019 Katsu Matsuda. All rights reserved.
+//
+
+import Foundation
+
+struct MRect {
+
+    var x: Float
+
+    var y: Float
+
+    var width: Float
+
+    var height: Float
+
+}
