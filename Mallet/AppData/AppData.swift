19a79d0d59c466ceb54576ea2353905bd891a7a6
Katsu Matsuda <kats.mtd@gmail.com>
2020-03-02 16:56:38 +0900

    Add AppData

---
+++ Mallet/AppData/AppData.swift
@@ -0,0 +1,19 @@
+//
+//  AppData.swift
+//  Mallet
+//  
+//  Created by Katsu Matsuda on 2020/03/02.
+//  Copyright (c) 2020 Katsu Matsuda. All rights reserved.
+//
+
+import Foundation
+
+class AppData: Codable {
+
+    let appName: String
+
+    let appID: Int
+
+    let uiData: [MUI]
+
+}
