a1a0f20ade3d3b9418e73b7dbbdb87a6791ee560
Katsu Matsuda <kats.mtd@gmail.com>
2020-03-15 00:29:00 +0900

    Add BoolInputCell

---
+++ Mallet/View/Editor/UIEditor/InputCell/BoolInputCell.swift
@@ -0,0 +1,31 @@
+//
+//  BoolInputCell.swift
+//  Mallet
+//  
+//  Created by Katsu Matsuda on 2020/03/15.
+//  Copyright (c) 2020 Katsu Matsuda. All rights reserved.
+//
+
+import SwiftUI
+
+struct BoolInputCell: View {
+
+    @Binding private var value: Bool
+
+    private let title: String
+
+    init(value: Binding<Bool>, title: String) {
+        self._value = value
+
+        self.title = title
+    }
+
+    var body: some View {
+        HStack {
+            Text(title)
+            Spacer()
+            Toggle("", isOn: $value)
+                .labelsHidden()
+        }
+    }
+}
