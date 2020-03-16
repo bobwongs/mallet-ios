646099fb46a8d3c7375902ba18ccf21fdde0032f
Katsu Matsuda <kats.mtd@gmail.com>
2020-03-16 21:52:57 +0900

    Add Today Widget

---
+++ MalletWidget/TodayViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TodayViewController.swift
+//  MalletWidget
+//
+//  Created by Katsu Matsuda on 2020/03/16.
+//  Copyright © 2020 Katsu Matsuda. All rights reserved.
+//
+
+import UIKit
+import NotificationCenter
+
+class TodayViewController: UIViewController, NCWidgetProviding {
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+        
+    func widgetPerformUpdate(completionHandler: (@escaping (NCUpdateResult) -> Void)) {
+        // Perform any setup necessary in order to update the view.
+        
+        // If an error is encountered, use NCUpdateResult.Failed
+        // If there's no update required, use NCUpdateResult.NoData
+        // If there's an update, use NCUpdateResult.NewData
+        
+        completionHandler(NCUpdateResult.newData)
+    }
+    
+}
