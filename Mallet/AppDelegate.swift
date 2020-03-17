bebb9a25291028dc7a506d2ee0ed6ea7a17873f1
Katsu Matsuda <kats.mtd@gmail.com>
2020-03-17 22:55:03 +0900

    Fix AppDelegate

--- Mallet/AppDelegate.swift
+++ Mallet/AppDelegate.swift
@@ -15,9 +15,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
 
-        UITableView.appearance().backgroundColor = .clear
-        UITableViewCell.appearance().backgroundColor = .clear
-
         addShortcuts(application: application)
 
         return true
