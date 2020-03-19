4badebca72c2da1b5eda7b83447cf5f383aacbbb
Katsu Matsuda <kats.mtd@gmail.com>
2020-03-19 19:54:00 +0900

    Add Storage

---
+++ Mallet/Storage/Storage.swift
@@ -0,0 +1,122 @@
+//
+//  Storage.swift
+//  Mallet
+//  
+//  Created by Katsu Matsuda on 2020/03/19.
+//  Copyright (c) 2020 Katsu Matsuda. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class AppRealmObject: Object {
+
+    @objc dynamic var appID = 0
+
+    @objc dynamic var appName = ""
+
+    var uiIDs = List<Int>()
+
+    @objc dynamic var uiDataJson = ""
+
+}
+
+class Storage {
+
+    static let realm = try! Realm()
+
+    static var maxAppID: Int {
+        realm.objects(AppRealmObject.self).max(ofProperty: "appID") ?? 0
+    }
+
+
+    static func createNewApp() -> AppData {
+        let appID = maxAppID + 1
+        let appName = "New App"
+        let uiIDs = [Int]()
+        let uiData = Dictionary<Int, MUI>()
+
+        let appData = AppData(appID: appID, appName: appName, uiIDs: uiIDs, uiData: uiData)
+
+        saveApp(appData: appData)
+
+        return appData
+    }
+
+    static func loadApp(appID: Int) -> AppData {
+        guard let appRealmObject = realm.objects(AppRealmObject.self).filter("appID == \(appID)").first else {
+            print("failed to load app")
+            return createNewApp()
+        }
+
+        let appName = appRealmObject.appName
+        var uiIDs = [Int]()
+        appRealmObject.uiIDs.forEach({ uiIDs.append($0) })
+
+        guard let uiData = AppData.json2UIData(jsonStr: appRealmObject.uiDataJson) else {
+            print("failed to load app")
+            return createNewApp()
+        }
+
+        return AppData(appID: appID, appName: appName, uiIDs: uiIDs, uiData: uiData)
+    }
+
+    static func saveApp(appData: AppData) {
+        guard let uiDataJson = AppData.uiData2Json(uiData: appData.uiData) else {
+            print("failed to save app")
+            return
+        }
+
+        DispatchQueue.global().async {
+            autoreleasepool {
+                if let appRealmObject = realm.objects(AppRealmObject.self).filter("appID == \(appData.appID)").first {
+                    do {
+                        try realm.write {
+                            appRealmObject.appName = appData.appName
+                            appRealmObject.uiDataJson = uiDataJson
+
+                            let uiIDs = List<Int>()
+                            appData.uiIDs.forEach({ uiIDs.append($0) })
+                            appRealmObject.uiIDs = uiIDs
+                        }
+                    } catch {
+                        print(error.localizedDescription)
+                    }
+                } else {
+                    let appRealmObject = AppRealmObject()
+                    appRealmObject.appID = appData.appID
+                    appRealmObject.appName = appData.appName
+                    appData.uiIDs.forEach({ appRealmObject.uiIDs.append($0) })
+                    appRealmObject.uiDataJson = uiDataJson
+
+                    do {
+                        try realm.write {
+                            realm.add(appRealmObject)
+                        }
+                    } catch {
+                        print(error.localizedDescription)
+                    }
+                }
+            }
+        }
+    }
+
+    static func deleteApp(appID: Int) {
+        DispatchQueue.global().async {
+            autoreleasepool {
+                guard  let appRealmObject = realm.objects(AppRealmObject.self).filter("appID == \(appID)").first else {
+                    return
+                }
+
+                do {
+                    try realm.write {
+                        realm.delete(appRealmObject)
+                    }
+                } catch {
+                    print(error.localizedDescription)
+                }
+            }
+        }
+    }
+
+}
