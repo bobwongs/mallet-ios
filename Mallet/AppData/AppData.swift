8df88e63255ac05988c1716f9dbdaa809291df97
Katsu Matsuda <kats.mtd@gmail.com>
2020-03-19 17:02:37 +0900

    Add AppData.uiData2Json() AppData.json2UIData()

--- Mallet/AppData/AppData.swift
+++ Mallet/AppData/AppData.swift
@@ -10,12 +10,45 @@ import Foundation
 
 class AppData: Codable {
 
-    let appName: String
-
     let appID: Int
+
+    let appName: String
 
     let uiIDs: [Int]
 
     let uiData: Dictionary<Int, MUI>
+
+    init(appID: Int, appName: String, uiIDs: [Int], uiData: Dictionary<Int, MUI>) {
+        self.appID = appID
+        self.appName = appName
+        self.uiIDs = uiIDs
+        self.uiData = uiData
+    }
+
+    static func uiData2Json(uiData: Dictionary<Int, MUI>) -> String? {
+        do {
+            do {
+                let data = try JSONEncoder().encode(uiData)
+                return String(data: data, encoding: .utf8)
+            } catch {
+                print(error.localizedDescription)
+                return nil
+            }
+        }
+    }
+
+    static func Json2UIData(jsonStr: String) -> Dictionary<Int, MUI>? {
+        guard let data = jsonStr.data(using: .utf8) else {
+            return nil
+        }
+
+        do {
+            let dictionary = try JSONDecoder().decode([Int: MUI].self, from: data)
+            return dictionary
+        } catch {
+            print(error.localizedDescription)
+            return nil
+        }
+    }
 
 }
