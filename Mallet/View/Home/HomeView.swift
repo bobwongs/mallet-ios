b0a86cebfb1a384b0c16f4e41280fdf5fd80e464
Katsu Matsuda <kats.mtd@gmail.com>
2020-03-21 18:58:24 +0900

    Update HomeView

--- Mallet/View/Home/HomeView.swift
+++ Mallet/View/Home/HomeView.swift
@@ -17,47 +17,78 @@ struct HomeView: View {
     @State private var showingEditor = false
 
     var body: some View {
-        VStack {
-            List {
-                ForEach(self.homeViewModel.apps, id: \.appID) { (app: HomeViewModel.AppInfo) in
-                    HomeAppCell(appID: app.appID,
-                                appName: app.appName,
-                                runApp: { print("Run App") },
-                                openEditor: { self.openEditor(appID: app.appID) },
-                                deleteApp: { Storage.deleteApp(appID: app.appID) }
-                    )
-                }
-                    .onMove { (idxs, to) in
-                        for idx in idxs {
-                            if idx < to {
-                                Storage.moveApp(idx, to - 1)
-                            } else {
-                                Storage.moveApp(idx, to)
-                            }
-                        }
-                    }
-                    .onDelete { idxs in
-                        for idx in idxs {
-                            Storage.deleteApp(appID: self.homeViewModel.apps[idx].appID)
-                        }
-                    }
-            }
-                .navigationBarTitle("My Apps", displayMode: .large)
-                .navigationBarItems(leading:
-                                    EditButton(),
-                                    trailing:
-                                    HStack {
-                                        Button(action: {
-                                            Storage.createNewApp()
-                                        }) {
-                                            Image(systemName: "plus")
-                                        }
-                                    }
-                )
-
+        ZStack {
             NavigationLink(destination: EditorView(closeEditor: { self.closeEditor() }).environmentObject(editorViewModel),
                            isActive: $showingEditor,
                            label: { EmptyView() })
+
+            appList()
+
+            VStack {
+                Spacer()
+                HStack {
+                    Spacer()
+                    Button(action: {
+                        self.editorViewModel = EditorViewModel(Storage.createNewApp())
+                        self.showingEditor = true
+                    }) {
+                        Circle()
+                            .foregroundColor(.blue)
+                            .overlay(
+                                Image(systemName: "plus")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: 20, height: 20)
+                                    .foregroundColor(.white)
+                            )
+                            .frame(width: 50, height: 50)
+                            .padding(.trailing, 20)
+                    }
+                }
+            }
+        }
+            .navigationBarTitle("My Apps", displayMode: .large)
+            .navigationBarItems(leading:
+                                EditButton(),
+                                trailing:
+                                HStack {
+                                    Button(action: {
+                                        print("Settings")
+                                    }) {
+                                        Image(systemName: "gear")
+                                            .padding(10)
+                                    }
+                                }
+            )
+    }
+
+    private func appList() -> some View {
+        List {
+            ForEach(self.homeViewModel.apps, id: \.appID) { (app: HomeViewModel.AppInfo) in
+                HomeAppCell(appID: app.appID,
+                            appName: app.appName,
+                            runApp: { print("Run App") },
+                            openEditor: { self.openEditor(appID: app.appID) },
+                            deleteApp: { Storage.deleteApp(appID: app.appID) }
+                )
+            }
+                .onMove { (idxs, to) in
+                    for idx in idxs {
+                        if idx < to {
+                            Storage.moveApp(idx, to - 1)
+                        } else {
+                            Storage.moveApp(idx, to)
+                        }
+                    }
+                }
+                .onDelete { idxs in
+                    for idx in idxs {
+                        Storage.deleteApp(appID: self.homeViewModel.apps[idx].appID)
+                    }
+                }
+
+            Spacer()
+                .frame(height: 80)
         }
     }
 
