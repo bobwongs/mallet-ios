d2eb916607533fa53303df45a9f2f168f6061643
Katsu Matsuda <kats.mtd@gmail.com>
2020-05-03 23:17:46 +0900

    Add AppViewModel

---
+++ Mallet/ViewModel/AppViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  AppViewModel.swift
+//  Mallet
+//  
+//  Created by Katsu Matsuda on 2020/05/03.
+//  Copyright (c) 2020 Katsu Matsuda. All rights reserved.
+//
+
+import SwiftUI
+
+class AppViewModel: ObservableObject {
+
+}
