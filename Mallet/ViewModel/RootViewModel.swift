ef2e78e947e0955e4cd663cf345052ae177336de
Katsu Matsuda <kats.mtd@gmail.com>
2020-05-04 17:51:04 +0900

    Add RootViewModel

---
+++ Mallet/ViewModel/RootViewModel.swift
@@ -0,0 +1,23 @@
+//
+//  RootViewModel.swift
+//  Mallet
+//  
+//  Created by Katsu Matsuda on 2020/05/04.
+//  Copyright (c) 2020 Katsu Matsuda. All rights reserved.
+//
+
+import Foundation
+
+class RootViewModel: ObservableObject {
+
+    @Published var runningApps = [AppViewModel]()
+
+    func runApp(id: Int) {
+
+    }
+
+    func exitApp(id: Int) {
+
+    }
+
+}
