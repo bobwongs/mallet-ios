459fe5baa91cb8d8bbd79f17c9c41387729fc7e1
Katsu Matsuda <kats.mtd@gmail.com>
2020-05-04 18:03:43 +0900

    Update AppViewModel

--- Mallet/ViewModel/AppViewModel.swift
+++ Mallet/ViewModel/AppViewModel.swift
@@ -10,4 +10,39 @@ import SwiftUI
 
 class AppViewModel: ObservableObject {
 
+    let appName: String
+
+    let appId: Int
+
+    let uiIDs: [Int]
+
+    @Published var uiData: Dictionary<Int, MUI>
+
+    init(_ appData: AppData) {
+        self.appName = appData.appName
+        self.appId = appData.appID
+        self.uiIDs = appData.uiIDs
+        self.uiData = appData.uiData
+    }
+
+    func getUIDataOf(_ id: Int) -> Binding<MUI> {
+        Binding(
+            get: { self.uiData[id] ?? MUI.none },
+            set: { self.uiData[id] = $0 }
+        )
+    }
+}
+
+extension AppViewModel: Hashable {
+    static func ==(lhs: AppViewModel, rhs: AppViewModel) -> Bool {
+        lhs.appName == rhs.appName &&
+        lhs.appId == rhs.appId &&
+        lhs.uiIDs == rhs.uiIDs
+    }
+
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(appName)
+        hasher.combine(appId)
+        hasher.combine(uiIDs)
+    }
 }
