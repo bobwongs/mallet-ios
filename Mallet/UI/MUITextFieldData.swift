152f31b13dd2ecb3a570a9166cb2a78ccea2c4ed
Katsu Matsuda <kats.mtd@gmail.com>
2020-05-06 22:52:24 +0900

    Add MUITextFieldController

--- Mallet/UI/MUITextFieldData.swift
+++ Mallet/UI/MUITextFieldData.swift
@@ -7,6 +7,7 @@
 //
 
 import SwiftUI
+import XyloSwift
 
 struct MUITextFieldData: Codable {
 
@@ -25,5 +26,41 @@ struct MUITextFieldData: Codable {
     static let disabled = MUITextFieldData(enabled: false, text: "", placeholder: "", color: .clear, size: 0, alignment: .center)
 
     static let defaulValue = MUITextFieldData(text: "", placeholder: "Text", color: .black, size: 17, alignment: .leading)
+
+}
+
+protocol MUITextFieldController {
+
+    // Implemented in MUITextController
+    // func setText(args: [XyObj]) -> XyObj
+
+    // Implemented in MUITextController
+    // func getText(args: [XyObj]) -> XyObj
+
+    /// - Parameter args: [UIID, Text(String)]
+    /// - Returns: .zero
+    func setPlaceHolder(args: [XyObj]) -> XyObj
+
+    /// - Parameter args: [UIID]
+    /// - Returns: Text(String)
+    func getPlaceHolder(args: [XyObj]) -> XyObj
+
+    // Implemented in MUITextController
+    // func setTextColor(args: [XyObj]) -> XyObj
+
+    // Implemented in MUITextController
+    // func getTextColor(args: [XyObj]) -> XyObj
+
+    // Implemented in MUITextController
+    // func setTextSize(args: [XyObj]) -> XyObj
+
+    // Implemented in MUITextController
+    // func getTextSize(args: [XyObj]) -> XyObj
+
+    // Implemented in MUITextController
+    // func setTextAlignment(args: [XyObj]) -> XyObj
+
+    // Implemented in MUITextController
+    // func getTextAlignment(args: [XyObj]) -> XyObj
 
 }
--- Mallet/ViewModel/App/UIController/AppViewModel+MUITextFieldController.swift
+++ Mallet/ViewModel/App/UIController/AppViewModel+MUITextFieldController.swift
@@ -9,49 +9,15 @@
 import SwiftUI
 import XyloSwift
 
-/*
 extension AppViewModel: MUITextFieldController {
 
-    func setTextField(args: [XyObj]) -> XyObj {
-
-        fatalError("setText(args:) has not been implemented")
-    }
-
-    func getTextField(args: [XyObj]) -> XyObj {
-        fatalError("getText(args:) has not been implemented")
-    }
-
     func setPlaceHolder(args: [XyObj]) -> XyObj {
-        fatalError("setPlaceHolder(args:) has not been implemented")
+        getUIData(args[0]).textFieldData.text.wrappedValue = args[1].string()
+        return .zero
     }
 
     func getPlaceHolder(args: [XyObj]) -> XyObj {
-        fatalError("getPlaceHolder(args:) has not been implemented")
-    }
-
-    func setTextFieldColor(args: [XyObj]) -> XyObj {
-        fatalError("setTextColor(args:) has not been implemented")
-    }
-
-    func getTextFieldColor(args: [XyObj]) -> XyObj {
-        fatalError("getTextColor(args:) has not been implemented")
-    }
-
-    func setTextFieldSize(args: [XyObj]) -> XyObj {
-        fatalError("setTextSize(args:) has not been implemented")
-    }
-
-    func getTextFieldSize(args: [XyObj]) -> XyObj {
-        fatalError("getTextSize(args:) has not been implemented")
-    }
-
-    func setTextFieldAlignment(args: [XyObj]) -> XyObj {
-        fatalError("setAlignment(args:) has not been implemented")
-    }
-
-    func getTextFieldAlignment(args: [XyObj]) -> XyObj {
-        fatalError("getAlignment(args:) has not been implemented")
+        XyObj(getUIData(args[0]).textFieldData.text.wrappedValue)
     }
 
 }
-*/
