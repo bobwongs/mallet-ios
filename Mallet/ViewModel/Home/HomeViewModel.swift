39aecd82ab4e343a716c9311543c18c8d6bf4f7c
Katsu Matsuda <kats.mtd@gmail.com>
2020-05-09 20:51:51 +0900

    Add debug code

--- Mallet/ViewModel/Home/HomeViewModel.swift
+++ Mallet/ViewModel/Home/HomeViewModel.swift
@@ -27,6 +27,10 @@ class HomeViewModel: ObservableObject {
     private var notificationTokens: [NotificationToken] = []
 
     init() {
+        #if DEBUG
+            print(Realm.Configuration.defaultConfiguration.fileURL)
+        #endif
+
         self.editorViewModel = EditorViewModel()
 
         let appLists = Storage.allAppLists()
