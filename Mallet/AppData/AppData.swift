d7024dbf3dfd5b02295e7b5ecd9cb460faff87e1
Katsu Matsuda <kats.mtd@gmail.com>
2020-05-10 22:21:24 +0900

    Add AppData.appData2Json() AppData.json2AppData()

--- Mallet/AppData/AppData.swift
+++ Mallet/AppData/AppData.swift
@@ -27,13 +27,11 @@ class AppData: Codable {
 
     static func uiData2Json(uiData: Dictionary<Int, MUI>) -> String? {
         do {
-            do {
-                let data = try JSONEncoder().encode(uiData)
-                return String(data: data, encoding: .utf8)
-            } catch {
-                print(error.localizedDescription)
-                return nil
-            }
+            let data = try JSONEncoder().encode(uiData)
+            return String(data: data, encoding: .utf8)
+        } catch {
+            print(error.localizedDescription)
+            return nil
         }
     }
 
@@ -48,6 +46,33 @@ class AppData: Codable {
         do {
             let dictionary = try JSONDecoder().decode([Int: MUI].self, from: json)
             return dictionary
+        } catch {
+            print(error.localizedDescription)
+            return nil
+        }
+    }
+
+    static func appData2Json(appData: AppData) -> String? {
+        do {
+            let data = try JSONEncoder().encode(appData)
+            return String(data: data, encoding: .utf8)
+        } catch {
+            print(error.localizedDescription)
+            return nil
+        }
+    }
+
+    static func json2AppData(jsonStr: String) -> AppData? {
+        guard  let json = jsonStr.data(using: .utf8) else {
+            return nil
+        }
+        return json2AppData(json: json)
+    }
+
+    static func json2AppData(json: Data) -> AppData? {
+        do {
+            let appData = try JSONDecoder().decode(AppData.self, from: json)
+            return appData
         } catch {
             print(error.localizedDescription)
             return nil
