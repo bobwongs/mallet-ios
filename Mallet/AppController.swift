4989687e751a8f9e5da8f013cf04d860db9a1c3d
Katsu Matsuda <kats.mtd@gmail.com>
2020-05-10 22:22:23 +0900

    Add AppController.installApp()

--- Mallet/AppController.swift
+++ Mallet/AppController.swift
@@ -20,4 +20,25 @@ class AppController {
         rootViewModel.exitApp(id: id)
     }
 
+    ///
+    /// - Parameters:
+    ///   - base64Str: encoded app data
+    ///   - sync: wait until app is installed
+    /// - Returns: app ID
+    static func installApp(base64Str: String, sync: Bool = false) -> Int? {
+        guard let data = Data(base64Encoded: base64Str) else {
+            print("failed to install app")
+            return nil
+        }
+
+        guard let appData = AppData.json2AppData(json: data) else {
+            print("failed to install app")
+            return nil
+        }
+
+        let appID = Storage.installApp(appData: appData, sync: sync).appID
+
+        return appID
+    }
+
 }
