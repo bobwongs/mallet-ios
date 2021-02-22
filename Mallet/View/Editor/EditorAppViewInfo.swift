38952dd8bd517460f366c9b1437dace995fed2fa
Katsu Matsuda <kats.mtd@gmail.com>
2021-02-22 21:57:22 +0900

    Add EditorAppViewInfo.frameSize()

--- Mallet/View/Editor/EditorAppViewInfo.swift
+++ Mallet/View/Editor/EditorAppViewInfo.swift
@@ -22,8 +22,14 @@ struct EditorAppViewInfo {
         (screenSize.width - gridRectSize * CGFloat(gridWidth)) / CGFloat(gridWidth * 2)
     }
 
-    static func gridSpacerHeight(screenSize: CGSize, rectPadding : CGFloat) -> CGFloat {
+    static func gridSpacerHeight(screenSize: CGSize, rectPadding: CGFloat) -> CGFloat {
         screenSize.height - (gridRectSize + rectPadding * 2) * CGFloat(gridHeight1 + gridHeight2)
+    }
+
+    static func frameSize(gridWidth: Int, gridHeight: Int, rectPadding: CGFloat) -> CGSize {
+        let width = (gridRectSize + rectPadding * 2) * CGFloat(gridWidth) - rectPadding * 2
+        let height = (gridRectSize + rectPadding * 2) * CGFloat(gridHeight) - rectPadding * 2
+        return CGSize(width: width, height: height)
     }
 
 }
